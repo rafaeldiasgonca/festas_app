77deae4638c9704c481c95b94ed8d9a0c50fe84e
Vinícius Pinheiro <vs_pinheiro@icloud.com>
2020-09-23 15:47:12 -0400

    De volta a versão livre de bugs

--- festas_app/ViewController.swift
+++ festas_app/ViewController.swift
@@ -20,14 +20,5 @@ class ViewController: UIViewController {
         self.navigationController!.navigationBar.isTranslucent = true
     }
 
-    @IBAction func startCardAnimated(_ sender: UIButton) {
-        stardCardButton.backgroundColor = .black
-        stardCardButton.alpha = 0.5
-    }
-    
-    @IBAction func startCardPressed(_ sender: UIButton) {
-        stardCardButton.alpha = 0.0
-    }
-
 }
 
