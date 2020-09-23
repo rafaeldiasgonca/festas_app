ab792de9c94f3295737cc04ff28fc7d10630cd8a
Vinícius Pinheiro <vs_pinheiro@icloud.com>
2020-09-23 13:50:33 -0400

    Arredondamento do card e inicio da função de animação

--- festas_app/ViewController.swift
+++ festas_app/ViewController.swift
@@ -10,14 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startCard: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        startCard.layer.cornerRadius = 12
         self.navigationController?.navigationBar.prefersLargeTitles = true
         self.navigationController!.navigationBar.isTranslucent = true
     }
-    
 
+    @IBAction func startCardAnimated(_ sender: UIButton) {
+        
+    }
+    
 }
 
