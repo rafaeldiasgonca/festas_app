b9322dee5996ae3b031c8562db7614c3c4858cf9
rafaeldiasgonca <rafaeldiasgonca@gmail.com.br>
2020-09-23 10:56:13 -0400

    adicionei a tela de modelos e estava começando a tela de organização

---
+++ festas_app/ViewControllerConfiguracao.swift
@@ -0,0 +1,22 @@
+//
+//  ViewControllerConfiguracao.swift
+//  festas_app
+//
+//  Created by Rafael Dias Gonçalves on 22/09/20.
+//  Copyright © 2020 Rafael Dias Gonçalves. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerConfiguracao: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+
+    
+
+}
