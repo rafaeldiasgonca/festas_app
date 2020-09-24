0a59037bcc8894ccd88dcf71aebff9eff95eaccf
rafaeldiasgonca <rafaeldiasgonca@gmail.com.br>
2020-09-24 14:36:42 -0400

    datePIcker da data

--- festas_app/ViewControllerConfiguracao.swift
+++ festas_app/ViewControllerConfiguracao.swift
@@ -11,13 +11,41 @@ import UIKit
 
 
 class ViewControllerConfiguracao: UIViewController {
+    @IBOutlet weak var dateTextField: UITextField!
+    
+    let datePicker = UIDatePicker()
+    
     var tituloRecebido = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = tituloRecebido        
+        self.title = tituloRecebido
+        createDatePickerView()
     }
     
-
+    func createDatePickerView(){
+        // Create toolBar
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        //Create bar Button
+        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action:#selector(donePressed))
+        
+        //assign done button to toolbar
+        toolBar.setItems([doneBtn], animated: true)
+        //assign toolbar to textfield
+        dateTextField.inputAccessoryView = toolBar
+        //assign datePicker to textField
+        dateTextField.inputView = datePicker
+        datePicker.datePickerMode  = .date
+    }
+    @objc func donePressed(){
+        let formatacao = DateFormatter()
+        formatacao.dateStyle = .long
+        formatacao.timeStyle = .none
+        dateTextField.text = formatacao.string(from: datePicker.date)
+        self.view.endEditing(true)
+        
+    }
     
 
 }
