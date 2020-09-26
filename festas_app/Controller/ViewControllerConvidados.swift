21f64779e5c6d69135835756d2ac4a044117db18
rafaeldiasgonca <rafaeldiasgonca@gmail.com.br>
2020-09-26 16:35:39 -0400

    Botão de adicionar funcionando Botão de excluir funcionado Adicionado arquivo para mexer em cada outlet de cada célula da tableView

--- festas_app/Controller/ViewControllerConvidados.swift
+++ festas_app/Controller/ViewControllerConvidados.swift
@@ -10,48 +10,53 @@ import UIKit
 
 class ViewControllerConvidados: UIViewController,UITableViewDataSource{
     @IBOutlet weak var tableView: UITableView!
-    
-    
-    var b = Int()
-    let azul =  ["abc","abcd"]
-    
+    var convidados1 = 1
+    var convidados:[String] = [""]
     @IBOutlet weak var ViewConvidados: UIView!
-    @IBOutlet weak var textFieldConvidados: UITextField!
+    
     var numDeConvidados = Int()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Convidados"
         ViewConvidados.layer.cornerRadius = 12
-        textFieldConvidados.layer.cornerRadius = 8
-        btnNumberPad()
+       
+    }
+    
+    
+    
+    @IBAction func addButton(_ sender: Any) {
+        inserirNovoConvidado()
         
     }
     
-    func btnNumberPad(){
-        let toolbar = UIToolbar()
-        toolbar.sizeToFit()
-        let dnButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action:#selector(dnButtonPressed))
-        toolbar.setItems([dnButton], animated: true)
-        textFieldConvidados.inputAccessoryView = toolbar
-    }
-    @objc func dnButtonPressed(){
-        self.view.endEditing(true)
-         b = Int(textFieldConvidados.text!) ?? 0
-        tableView.reloadData()
-        
-        
+    func inserirNovoConvidado(){
+        convidados1 = convidados1 + 1
+        let indexPath = IndexPath.init(row: convidados1 - 1, section: 0)
+        tableView.beginUpdates()
+        tableView.insertRows(at:[indexPath], with:.automatic)
+        tableView.endUpdates()
     }
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return b
+        return convidados1
         
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier:"Convidados", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier:"Convidados", for: indexPath) as! NomeDosConvidadosTableViewCell
+       // convidados.append(cell.NomeDosConvidadosTf.text ?? "")
         return cell
     }
-    
-    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            convidados1 = convidados1 - 1
+            tableView.beginUpdates()
+            tableView.deleteRows(at:[indexPath], with: .automatic)
+            tableView.endUpdates()
+            
+        }
+    }
 }
---
+++ festas_app/View/NomeDosConvidadosTableViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  NomeDosConvidadosTableViewCell.swift
+//  festas_app
+//
+//  Created by Rafael Dias Gonçalves on 26/09/20.
+//  Copyright © 2020 Rafael Dias Gonçalves. All rights reserved.
+//
+
+import UIKit
+
+class NomeDosConvidadosTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var NomeDosConvidadosTf: UITextField!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+       createToolBar()
+        
+    }
+    
+    @IBAction func NomeDosConvidadosAC(_ sender: Any) {
+        
+    }
+    func createToolBar(){
+          
+                     let toolbar = UIToolbar()
+                     toolbar.sizeToFit()
+                     let dnButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(dnbuttonPressed))
+                     toolbar.setItems([dnButton], animated: true)
+           NomeDosConvidadosTf.inputAccessoryView = toolbar
+              
+          }
+       @objc func dnbuttonPressed(){
+           let vc = ViewControllerConvidados()
+        vc.convidados.append(NomeDosConvidadosTf.text ?? "")
+              
+          }
+   
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+   
+
+}
