4001046aa289df3813cc1a33b222e02d8de6ee30
rafaeldiasgonca <rafaeldiasgonca@gmail.com.br>
2020-10-01 16:55:00 -0400

    tela de inicio montada

--- festas_app/Controller/ViewControllerInicio.swift
+++ festas_app/Controller/ViewControllerInicio.swift
@@ -8,19 +8,32 @@
 
 import UIKit
 
-class ViewControllerInicio: UIViewController {
-
+class ViewControllerInicio: UIViewController,UITableViewDataSource{
+    
+    
+    var itens:[String] = ["","",""]
+    @IBOutlet weak var viewPrincipal: UIView!
     @IBOutlet weak var viewDetalhesData: UIView!
     @IBOutlet weak var viewDetalhes: UIView!
+    @IBOutlet weak var tableViewDetalhes: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "ola,festeiro"
+        self.title = "Ola,festeiro"
         navigationItem.largeTitleDisplayMode = .always
-
-        
+        self.viewDetalhes.layer.cornerRadius = 12
+        self.viewPrincipal.layer.cornerRadius = 12
+        self.viewDetalhesData.layer.cornerRadius = 12
     }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier:"itens", for: indexPath) as!ResumoDeItensTableViewCell
+        cell.LabelItens.text = itens[indexPath.row]
+        return cell
+    }
    
 
 }
---
+++ festas_app/Model/ResumoDeItensTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ResumoDeItensTableViewCell.swift
+//  festas_app
+//
+//  Created by Rafael Dias Gonçalves on 01/10/20.
+//  Copyright © 2020 Rafael Dias Gonçalves. All rights reserved.
+//
+
+import UIKit
+
+class ResumoDeItensTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var LabelItens: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+}
